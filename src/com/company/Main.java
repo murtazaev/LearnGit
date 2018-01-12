af87f990961238dd82a8db37f29583e01a25f98e
Abdulmutalim Murtazaev <abdulmutalim.murtazaev@mail.ru>
2018-01-12 16:57:33 +0400

    Тренировка с удаленными ветками

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.print("Код для проверки слияния");
+        System.out.print("Код для удаленной вторичной ветки");
     }
 }
