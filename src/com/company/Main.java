f65b6c092eca20cf947bd5c858251ce4c8a65ecf
Abdulmutalim Murtazaev <abdulmutalim.murtazaev@mail.ru>
2018-01-17 12:35:27 +0400

    Повторение

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Код для проверки слияния");
         System.out.print("Код для удаленной вторичной ветки");
+        System.out.print("Повторение");
     }
 }
